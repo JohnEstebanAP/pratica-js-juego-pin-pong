71dd449ad147533b073489af562baafca0172c47
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-06-06 15:05:16 -0500

    funcion para el play y pausa del juego, correción de bug memoria ram

--- pong/main.js
+++ pong/main.js
@@ -1,127 +1,147 @@
 (function () {
-  self.Board = function(width, height){
+  self.Board = function (width, height) {
     this.width = width;
     this.height = height;
     this.playing = false;
     this.game_over = false;
     this.bars = [];
     this.ball = null;
-  }
+    this.playing = false;
+  };
 
   self.Board.prototype = {
-    get elements(){
-      var elements = this.bars;
+    get elements() {
+      var elements = this.bars.map(function (bar) {
+        return bar;
+      });
       elements.push(this.ball);
       return elements;
-    }
-  }
+    },
+  };
 })();
-(function(){
-  self.Ball = function(x,y,radius,board){
+(function () {
+  self.Ball = function (x, y, radius, board) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.speed_y = 0;
     this.speed_x = 3;
     this.board = board;
+    this.direction = 1;
 
     board.ball = this;
     this.kind = "circle";
-  }
+  };
+
+  self.Ball.prototype = {
+    move: function () {
+      this.x += this.speed_x * this.direction;
+      this.y += this.speed_y;
+    },
+  };
 })();
-(function(){
-  self.Bar = function(x,y,width, height, board){
+(function () {
+  self.Bar = function (x, y, width, height, board) {
     this.x = x;
-    this.y =  y;
+    this.y = y;
     this.width = width;
     this.height = height;
     this.board = board;
     this.board.bars.push(this);
     this.kind = "rectangle";
-    this.speed= 10;
-  }
+    this.speed = 10;
+  };
 
   self.Bar.prototype = {
-    down: function(){
+    down: function () {
       this.y += this.speed;
     },
-    up: function(){
+    up: function () {
       this.y -= this.speed;
-    }
-  }
+    },
+  };
 })();
 
-
-
-(function (){
-  self.BoardView = function(canvas, board){
-
-    this.canvas =  canvas;
+(function () {
+  self.BoardView = function (canvas, board) {
+    this.canvas = canvas;
     this.canvas.width = board.width;
     this.canvas.height = board.height;
     this.board = board;
     this.ctx = canvas.getContext("2d");
-  }
+  };
 
   self.BoardView.prototype = {
-    clean: function(){
+    clean: function () {
       this.ctx.clearRect(0, 0, this.board.width, this.board.height);
     },
-    draw: function(){
-      for (var i  = this.board.elements.length -1 ;i >= 0; i--) {
+    draw: function () {
+      for (var i = this.board.elements.length - 1; i >= 0; i--) {
         var el = this.board.elements[i];
-        draw (this.ctx, el);
+        draw(this.ctx, el);
       }
     },
-    play: function(){
-      this.clean();
-      this.draw();
-    }
-  }
-
-  function draw(ctx, element){
-      switch(element.kind){
-        case "rectangle":
-          ctx.fillRect(element.x, element.y, element.width, element.height);
-          break;
-        case "circle":
-          ctx.beginPath();
-          ctx.arc(element.x, element.y, element.radius, 0, 7);
-          ctx.fill();
-          ctx.closePath();
-          break;
+    play: function () {
+      if(this.board.playing){
+        this.clean();
+        this.draw();
+        this.board.ball.move();  
       }
+    },
+  };
+
+  function draw(ctx, element) {
+    switch (element.kind) {
+      case "rectangle":
+        ctx.fillRect(element.x, element.y, element.width, element.height);
+        break;
+      case "circle":
+        ctx.beginPath();
+        ctx.arc(element.x, element.y, element.radius, 0, 7);
+        ctx.fill();
+        ctx.closePath();
+        break;
+    }
   }
 })();
 
+var board = new Board(800, 400);
+var bar1 = new Bar(20, 100, 40, 100, board);
+var bar2 = new Bar(740, 100, 40, 100, board);
+var canvas = document.getElementById("canvas");
+var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
 
-  var board = new Board(800,400);
-  var bar1 = new Bar(20, 100, 40, 100, board);
-  var bar2 = new Bar(740, 100, 40, 100, board);
-  var canvas = document.getElementById('canvas');
-  var board_view = new BoardView(canvas, board);
-  var ball = new Ball(350, 100, 10, board);
-
-
-document.addEventListener("keydown", function(ev){
-   ev.preventDefault();
-  if(ev.keyCode == 38 ){ //flecha hacia arriaba
-      bar2.up();
-  }
-  else if(ev.keyCode == 40 ){ //flecha hacia abajo
+document.addEventListener("keydown", function (ev) {
+  if (ev.keyCode === 38) {
+    //flecha hacia arriaba
+    ev.preventDefault();
+    bar2.up();
+  } else if (ev.keyCode === 40) {
+    //flecha hacia abajo
+    ev.preventDefault();
     bar2.down();
   }
-  if(ev.keyCode == 188 ){ //flecha hacia arriaba
-      bar1.up();
-  }
-  else if(ev.keyCode == 79){ //flecha hacia abajo
+  if (ev.keyCode === 188) {
+    //flecha hacia arriaba
+    ev.preventDefault();
+    bar1.up();
+  } else if (ev.keyCode === 79) {
+    //flecha hacia abajo
+    ev.preventDefault();
     bar1.down();
+  } else if (ev.keyCode === 32) {
+    //barra espaciadora
+    ev.preventDefault();
+    board.playing = !board.playing;
   }
 });
 
+//dibujamos el juego par pdimera ves
+board_view.draw();
 window.requestAnimationFrame(controller);
 
-function controller(){
+function controller() {
   board_view.play();
   window.requestAnimationFrame(controller);
 }
